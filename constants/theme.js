56d7c9e4529f44c7b6ac1d01eca5f4bf406f91c7
faniyolanda <faniyolanda0814@gmail.com>
2022-06-09 23:47:15 +0700

    theme

--- constants/theme.js
+++ constants/theme.js
@@ -1,32 +1,32 @@
-import { Dimensions } from "react-native";
-const { width, height } = Dimensions.get("window");
+import { Dimensions } from 'react-native';
+const { width, height } = Dimensions.get('window');
 
 export const COLORS = {
-    primary: "#FF0000", //red
-    transparentPrimray: "rgba(227, 120, 75, 0.4)",
-    orange: "#FFA133",
-    lightOrange: "#FFA133",
-    lightOrange2: "#FDDED4",
-    lightOrange3: "#FFD9AD",
-    green: "#27AE60",
-    red: "#FF1717",
-    blue: "#0064C0",
-    darkBlue: "#111A2C",
-    darkGray: "#525C67",
-    darkGray2: "#757D85",
-    gray: "#898B9A",
-    gray2: "#BBBDC1",
-    gray3: "#CFD0D7",
-    lightGray1: "#DDDDDD",
-    lightGray2: "#F5F5F8",
-    white2: "#FBFBFB",
-    white: "#FFFFFF",
-    black: "#000000",
+    primary: '#2DA94F', //green
+    transparentPrimray: 'rgba(227, 120, 75, 0.4)',
+    orange: '#FFA133',
+    lightOrange: '#FFA133',
+    lightOrange2: '#FDDED4',
+    lightOrange3: '#FFD9AD',
+    green: '#27AE60',
+    red: '#FF1717',
+    blue: '#0064C0',
+    darkBlue: '#111A2C',
+    darkGray: '#525C67',
+    darkGray2: '#757D85',
+    gray: '#898B9A',
+    gray2: '#BBBDC1',
+    gray3: '#CFD0D7',
+    lightGray1: '#DDDDDD',
+    lightGray2: '#F5F5F8',
+    white2: '#FBFBFB',
+    white: '#FFFFFF',
+    black: '#000000',
 
-    transparent: "transparent",
-    transparentBlack1: "rgba(0, 0, 0, 0.1)",
-    transparentBlack5: "rgba(0, 0, 0, 0.5)",
-    transparentBlack7: "rgba(0, 0, 0, 0.7)",
+    transparent: 'transparent',
+    transparentBlack1: 'rgba(0, 0, 0, 0.1)',
+    transparentBlack5: 'rgba(0, 0, 0, 0.5)',
+    transparentBlack7: 'rgba(0, 0, 0, 0.7)'
 };
 export const SIZES = {
     // global sizes
@@ -50,40 +50,40 @@ export const SIZES = {
 
     // app dimensions
     width,
-    height,
+    height
 };
 export const FONTS = {
-    largeTitle: { fontFamily: "Poppins-Black", fontSize: SIZES.largeTitle },
-    h1: { fontFamily: "Poppins-Bold", fontSize: SIZES.h1, lineHeight: 36 },
-    h2: { fontFamily: "Poppins-Bold", fontSize: SIZES.h2, lineHeight: 30 },
-    h3: { fontFamily: "Poppins-SemiBold", fontSize: SIZES.h3, lineHeight: 22 },
-    h4: { fontFamily: "Poppins-SemiBold", fontSize: SIZES.h4, lineHeight: 22 },
-    h5: { fontFamily: "Poppins-SemiBold", fontSize: SIZES.h5, lineHeight: 22 },
+    largeTitle: { fontFamily: 'Poppins-Black', fontSize: SIZES.largeTitle },
+    h1: { fontFamily: 'Poppins-Bold', fontSize: SIZES.h1, lineHeight: 36 },
+    h2: { fontFamily: 'Poppins-Bold', fontSize: SIZES.h2, lineHeight: 30 },
+    h3: { fontFamily: 'Poppins-SemiBold', fontSize: SIZES.h3, lineHeight: 22 },
+    h4: { fontFamily: 'Poppins-SemiBold', fontSize: SIZES.h4, lineHeight: 22 },
+    h5: { fontFamily: 'Poppins-SemiBold', fontSize: SIZES.h5, lineHeight: 22 },
     body1: {
-        fontFamily: "Poppins-Regular",
+        fontFamily: 'Poppins-Regular',
         fontSize: SIZES.body1,
-        lineHeight: 36,
+        lineHeight: 36
     },
     body2: {
-        fontFamily: "Poppins-Regular",
+        fontFamily: 'Poppins-Regular',
         fontSize: SIZES.body2,
-        lineHeight: 30,
+        lineHeight: 30
     },
     body3: {
-        fontFamily: "Poppins-Regular",
+        fontFamily: 'Poppins-Regular',
         fontSize: SIZES.body3,
-        lineHeight: 22,
+        lineHeight: 22
     },
     body4: {
-        fontFamily: "Poppins-Regular",
+        fontFamily: 'Poppins-Regular',
         fontSize: SIZES.body4,
-        lineHeight: 22,
+        lineHeight: 22
     },
     body5: {
-        fontFamily: "Poppins-Regular",
+        fontFamily: 'Poppins-Regular',
         fontSize: SIZES.body5,
-        lineHeight: 22,
-    },
+        lineHeight: 22
+    }
 };
 
 const appTheme = { COLORS, SIZES, FONTS };
